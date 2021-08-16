dfc7adee3c57891d03c919f1ab4845eb7484cc95
Tyler Rockwood <tyler@shortwave.com>
2021-08-16 11:04:07 -0500

    Don't import stuff we don't care about

--- quill.js
+++ quill.js
@@ -34,14 +34,14 @@ import Syntax from './modules/syntax';
 import Table from './modules/table';
 import Toolbar from './modules/toolbar';
 
-import Icons from './ui/icons';
-import Picker from './ui/picker';
-import ColorPicker from './ui/color-picker';
-import IconPicker from './ui/icon-picker';
-import Tooltip from './ui/tooltip';
+// import Icons from './ui/icons';
+// import Picker from './ui/picker';
+// import ColorPicker from './ui/color-picker';
+// import IconPicker from './ui/icon-picker';
+// import Tooltip from './ui/tooltip';
 
-import BubbleTheme from './themes/bubble';
-import SnowTheme from './themes/snow';
+// import BubbleTheme from './themes/bubble';
+// import SnowTheme from './themes/snow';
 
 Quill.register(
   {
@@ -96,14 +96,14 @@ Quill.register(
     'modules/table': Table,
     'modules/toolbar': Toolbar,
 
-    'themes/bubble': BubbleTheme,
-    'themes/snow': SnowTheme,
+    // 'themes/bubble': BubbleTheme,
+    // 'themes/snow': SnowTheme,
 
-    'ui/icons': Icons,
-    'ui/picker': Picker,
-    'ui/icon-picker': IconPicker,
-    'ui/color-picker': ColorPicker,
-    'ui/tooltip': Tooltip,
+    // 'ui/icons': Icons,
+    // 'ui/picker': Picker,
+    // 'ui/icon-picker': IconPicker,
+    // 'ui/color-picker': ColorPicker,
+    // 'ui/tooltip': Tooltip,
   },
   true,
 );
