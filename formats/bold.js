c87858ca40b56b8cc6f56891b0ce118e15573c39
Jonny Dimond <jonny@shortwave.com>
2021-12-05 18:42:21 -0800

    Support inline-style overrides for bold tags

--- formats/bold.js
+++ formats/bold.js
@@ -1,11 +1,19 @@
 import Inline from '../blots/inline';
 
+const NON_BOLD_FONT_WEIGHTS = ['normal', 'lighter'];
+
 class Bold extends Inline {
   static create() {
     return super.create();
   }
 
-  static formats() {
+  static formats(node) {
+    const fontWeightNumber = parseInt(node.style.fontWeight, 10);
+    const isNormalWeight =
+      fontWeightNumber <= 400 ||
+      NON_BOLD_FONT_WEIGHTS.includes(node.style.fontWeight);
+    // If this is a b-tag that is supposed to format normally, just return undefined so no format is not applied.
+    if (isNormalWeight) return undefined;
     return true;
   }
 
