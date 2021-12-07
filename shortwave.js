99879fd13cc983a69ebb32edcaba58db7d722057
Tyler Rockwood <tyler@shortwave.com>
2021-12-07 12:53:27 -0600

    Trim out more modules/formats that shortwave doesn't need

--- shortwave.js
+++ shortwave.js
@@ -1,72 +1,23 @@
 import Quill from './core';
-
-import { AlignClass, AlignStyle } from './formats/align';
-import {
-  DirectionAttribute,
-  DirectionClass,
-  DirectionStyle,
-} from './formats/direction';
 import Indent from './formats/indent';
 
 import Blockquote from './formats/blockquote';
 import Header from './formats/header';
 import List from './formats/list';
 
-import { BackgroundClass, BackgroundStyle } from './formats/background';
-import { ColorClass, ColorStyle } from './formats/color';
-import { FontClass, FontStyle } from './formats/font';
-import { SizeClass, SizeStyle } from './formats/size';
-
 import Bold from './formats/bold';
 import Italic from './formats/italic';
 import Link from './formats/link';
-import Script from './formats/script';
 import Strike from './formats/strike';
 import Underline from './formats/underline';
 
-import Formula from './formats/formula';
-import Image from './formats/image';
-import Video from './formats/video';
-
-import CodeBlock, { Code as InlineCode } from './formats/code';
-
-import Syntax from './modules/syntax';
-import Table from './modules/table';
-import Toolbar from './modules/toolbar';
+import CodeBlock, {Code as InlineCode} from './formats/code';
 
 // Only stuff that shortwave needs here.
 
 Quill.register(
   {
-    'attributors/attribute/direction': DirectionAttribute,
-
-    'attributors/class/align': AlignClass,
-    'attributors/class/background': BackgroundClass,
-    'attributors/class/color': ColorClass,
-    'attributors/class/direction': DirectionClass,
-    'attributors/class/font': FontClass,
-    'attributors/class/size': SizeClass,
-
-    'attributors/style/align': AlignStyle,
-    'attributors/style/background': BackgroundStyle,
-    'attributors/style/color': ColorStyle,
-    'attributors/style/direction': DirectionStyle,
-    'attributors/style/font': FontStyle,
-    'attributors/style/size': SizeStyle,
-  },
-  true,
-);
-
-Quill.register(
-  {
-    'formats/align': AlignClass,
-    'formats/direction': DirectionClass,
     'formats/indent': Indent,
-
-    'formats/background': BackgroundStyle,
-    'formats/color': ColorStyle,
-    'formats/font': FontClass,
-    'formats/size': SizeClass,
 
     'formats/blockquote': Blockquote,
     'formats/code-block': CodeBlock,
@@ -77,18 +28,8 @@ Quill.register(
     'formats/code': InlineCode,
     'formats/italic': Italic,
     'formats/link': Link,
-    'formats/script': Script,
     'formats/strike': Strike,
     'formats/underline': Underline,
-
-    'formats/formula': Formula,
-    'formats/image': Image,
-    'formats/video': Video,
-
-    'modules/syntax': Syntax,
-    'modules/table': Table,
-    'modules/toolbar': Toolbar,
-
   },
   true,
 );
